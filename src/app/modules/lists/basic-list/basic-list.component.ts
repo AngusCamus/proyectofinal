26e6bcd51ba9c1c7ce6cecaa4937a66f9b3e7e39
Brian Polesel <brianpolesel07@gmail.com>
2022-12-03 11:32:51 -0300

    finally class directivas

--- src/app/modules/lists/basic-list/basic-list.component.ts
+++ src/app/modules/lists/basic-list/basic-list.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+
+
+
+export type Producto = {
+  name: string,
+  price: number,
+  description: string
+}
 
 @Component({
   selector: 'app-basic-list',
@@ -6,5 +15,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic-list.component.scss']
 })
 export class BasicListComponent {
+  
+  elementsList: Producto[] = [
+    {
+      name: "Leche",
+      price: 24,
+      description: "Leche entera"
+    },
+    {
+      name: "Coca",
+      price: 28,
+      description: "Coca cola 2Lts"
+    }
+  ];
+  charging: boolean = false;
+  option: number = 0;
+  
+  
+  changeCharging(){
+    this.charging = !this.charging;
+  }
+
+  selectOption(option: number){
+    this.option = option;
+  }
 
 }
